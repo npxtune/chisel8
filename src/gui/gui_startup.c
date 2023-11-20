36320af00d8b42d5ed374d3f00ab3ac1f3db2e96
Tim <horochowiktim@pm.me>
2023-11-20 19:13:33 +0100

    add files

---
+++ src/gui/gui_startup.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "gui/gui_startup.h"
+
+int32_t startup_window(void) {
+    printf("test\n");
+    return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "gui/gui_startup.h"
+
+int main() {
+    startup_window();
+    return 0;
+}
